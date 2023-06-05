45c23b19d0640be1fcad60e29e27fe78811eb104
npoy <nicolas.poy@outlook.com>
2023-06-04 21:34:01 -0300

    Added Scriptable Objects example

---
+++ Assets/Scripts/Flyweight/Plant.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Flyweight;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField]
+    private PlantData info;
+    SetPlantInfo spi;
+
+    void Start() {
+        spi = GameObject.FindWithTag("PlantInfo").GetComponent<SetPlantInfo>();
+    }
+
+    void OnMouseDown() {
+        spi.OpenPlantPanel();
+        spi.plantName.text = info.Name;
+        spi.threatLevel.text = info.Threat.ToString();
+        spi.plantIcon.GetComponent<RawImage>().texture = info.Icon;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Player" && info.Threat == PlantData.THREAT.High) {
+            PlayerController.dead = true;
+        }
+    }
+}
