684c250d7b89c2e1264e0a0262822117d1d0d704
Nicolas Poy <nicolas.poy@corp.smtsoftware.com>
2023-06-13 23:50:01 -0300

    Added prototype example -WIP-

---
+++ Assets/Scripts/Prototype/Spawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject cubeprefab;
+        public GameObject sphereprefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Random.Range(0, 100) < 10) Instantiate(cubeprefab, this.transform.position, Quaternion.identity);
+            else if (Random.Range(0, 100) > 10) Instantiate(sphereprefab, this.transform.position, Quaternion.identity);
+        }
+    }
+}
+
